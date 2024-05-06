bc571cd3e0614769c85e9cffb237f5e8214293c8
Turobjon <avezovzamir337@gmail.com>
2024-05-06 14:57:58 +0500

    salom

---
+++ script.js
@@ -0,0 +1,17 @@
+let ism = prompt( ' ismingizni kiriting ' );
+
+let spaceid = prompt( ' space idingizni kiriting  kiriting ' );
+
+let spacepassword = prompt( ' space passwordingizni kiriting  kiriting ' );
+
+const javaScript = {
+    name: (ism),
+    space_id: (spaceid),    
+    space_password: (spacepassword)    
+
+}
+
+ 
+console.log(
+  javaScript
+);
